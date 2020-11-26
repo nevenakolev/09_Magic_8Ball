36391499ef54184f000b2e3543012ecd18a74b0e
nevena-kolev <nevena.kolev@htl-wels.at>
2020-11-26 14:05:16 +0100

    .xml files and further classes added, basic structure of each class is built (LoginController.java and MainController.java implement 'Initializeable' class

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * @author Nevena Kolev
+ * @version 1.0, 27.11.20
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+    public static void main(String[] args) {
+        //launches the JavaFX Application
+        launch(args);
+    }
+}
---
+++ src/login/LoginController.java
@@ -0,0 +1,12 @@
+package login;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
